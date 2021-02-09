b5986ea9ace39bce536fc3c2a67d2df8e0d72360
Vassilyev Anton <a.vasiljev.n@gmail.com>
2021-02-09 17:27:15 +0200

    main structure

---
+++ components/index/Slider.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="slider">
+        <a class="slider-item" href="#">
+            <img src="/carousel/banner1.jpg" alt="">
+        </a>
+        <a class="slider-item" href="#">
+            <img src="/carousel/banner2.jpg" alt="">
+        </a>
+        <a class="slider-item" href="#">
+            <img src="/carousel/banner3.jpg" alt="">
+        </a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Slider',
+        components: {}
+    }
+</script>
+
+<style></style>

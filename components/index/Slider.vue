740bffdc76f1a79a269ad341ca42c9604d9da6ef
Vassilyev Anton <a.vasiljev.n@gmail.com>
2021-02-09 17:57:50 +0200

    add slider

--- components/index/Slider.vue
+++ components/index/Slider.vue
@@ -1,22 +1,57 @@
 <template>
     <div class="slider">
-        <a class="slider-item" href="#">
-            <img src="/carousel/banner1.jpg" alt="">
-        </a>
-        <a class="slider-item" href="#">
-            <img src="/carousel/banner2.jpg" alt="">
-        </a>
-        <a class="slider-item" href="#">
-            <img src="/carousel/banner3.jpg" alt="">
-        </a>
+        <splide :options="options">
+            <splide-slide>
+                <a class="slider-item" href="#">
+                    <img src="/carousel/banner1.jpg" alt="">
+                </a>
+            </splide-slide>
+            <splide-slide>
+                <a class="slider-item" href="#">
+                    <img src="/carousel/banner2.jpg" alt="">
+                </a>
+            </splide-slide>
+            <splide-slide>
+                <a class="slider-item" href="#">
+                    <img src="/carousel/banner3.jpg" alt="">
+                </a>
+            </splide-slide>
+        </splide>
     </div>
 </template>
 
 <script>
+    import { Splide, SplideSlide } from '@splidejs/vue-splide';
+    import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+
     export default {
         name: 'Slider',
-        components: {}
+        components: {
+            Splide,
+            SplideSlide
+        },
+        data() {
+            return {
+                options: {
+                    type: 'fade',
+                    rewind: true,
+                    perPage: 1,
+                    autoplay: true,
+                    interval: 4000,
+                    navigation: false,
+                    speed: 500,
+                    arrows: false
+                }
+            }
+        }
     }
 </script>
 
-<style></style>
+<style>
+    .splide__pagination__page {
+        background: rgba(3, 3, 3, .3);
+    }
+    .splide__pagination__page.is-active {
+        background: #53B175;
+    }
+</style>

57561ff2c3d0775882719ef01ee3735dc66acca2
Vassilyev Anton <a.vasiljev.n@gmail.com>
2021-02-25 16:19:13 +0200

    errors

---
+++ layouts/error.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="top">
+            <div class="top_action"></div>
+            <div class="top_title">Error</div>
+            <div class="top_action"></div>
+        </div>
+        <div class="container">
+            <div v-if="error.statusCode === 404" class="message">Page not found</div>
+            <div v-else class="message">An error occurred</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['error']
+    }
+</script>
